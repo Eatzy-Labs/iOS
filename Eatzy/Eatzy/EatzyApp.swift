a1a69a8345e24ab2012039866c08f54cb3961cfd
sun <sts07190@naver.com>
2026-09-23 22:51:51 +0900

    [setting] 프로젝트 초기 세팅

---
+++ Eatzy/Eatzy/EatzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatzyApp.swift
+//  Eatzy
+//
+//  Created by sun on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct EatzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
