0b588b3f6819dcf0b787c97b5cadaf01ceac913f
sun <sts07190@naver.com>
2026-09-24 15:12:36 +0900

    [feat/#17] 이메일 정규식 추가

---
+++ Eatzy/Eatzy/Shared/Extensions/String+Validation.swift
@@ -0,0 +1,13 @@
+//
+//  String+Validation.swift
+//  Eatzy
+//
+
+import Foundation
+
+extension String {
+    var isValidEmail: Bool {
+        let pattern = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        return range(of: pattern, options: .regularExpression) != nil
+    }
+}
