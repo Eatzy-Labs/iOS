7d64d5cf04e645fc0a966e5143a679adc8d5921b
sun <sts07190@naver.com>
2026-09-24 13:56:20 +0900

    [feat/#15] 루트뷰에 온보딩 분기 추가

--- Eatzy/Eatzy/App/RootFeature.swift
+++ Eatzy/Eatzy/App/RootFeature.swift
@@ -2,6 +2,8 @@
 //  RootFeature.swift
 //  Eatzy
 //
+//  Created by sun on 9/24/26.
+//
 
 import ComposableArchitecture
 
@@ -11,10 +13,12 @@ struct RootFeature: Reducer {
         enum Route: Equatable {
             case splash
             case login
+            case onboarding
         }
 
         var route: Route = .splash
         var login = LoginFeature.State()
+        var onboarding = OnboardingFeature.State()
     }
 
     @CasePathable
@@ -22,6 +26,7 @@ struct RootFeature: Reducer {
         case splashTask
         case splashFinished
         case login(LoginFeature.Action)
+        case onboarding(OnboardingFeature.Action)
     }
 
     @Dependency(\.continuousClock) private var clock
@@ -30,23 +35,51 @@ struct RootFeature: Reducer {
         case splashDelay
     }
 
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {
-        switch action {
-        case .splashTask:
-            return .run { send in
-                try await clock.sleep(for: .seconds(2))
-                await send(.splashFinished)
-            }
-            .cancellable(id: CancelID.splashDelay, cancelInFlight: true)
+    var body: some Reducer<State, Action> {
+        Scope(state: \.login, action: \.login) {
+            LoginFeature()
+        }
 
-        case .splashFinished:
-            state.route = .login
+        Scope(state: \.onboarding, action: \.onboarding) {
+            OnboardingFeature()
+        }
+
+        Reduce { state, action in
+            switch action {
+            case .splashTask:
+                return .run { send in
+                    try await clock.sleep(for: .seconds(2))
+                    await send(.splashFinished)
+                }
+                .cancellable(id: CancelID.splashDelay, cancelInFlight: true)
+
+            case .splashFinished:
+                state.route = .login
+                return .none
+
+        case .login(.signUpButtonTapped):
+            state.onboarding = OnboardingFeature.State(entryPoint: .signUp)
+            state.route = .onboarding
             return .none
 
-        case let .login(loginAction):
-            return LoginFeature()
-                .reduce(into: &state.login, action: loginAction)
-                .map(Action.login)
+        case .login(.guestButtonTapped):
+            state.onboarding = OnboardingFeature.State(entryPoint: .guest)
+            state.route = .onboarding
+            return .none
+
+            case .login:
+                return .none
+
+            case .onboarding(.delegate(.backToLogin)):
+                state.route = .login
+                return .none
+
+            case .onboarding(.delegate(.onboardingCompleted)):
+                return .none
+
+            case .onboarding:
+                return .none
+            }
         }
     }
 }
